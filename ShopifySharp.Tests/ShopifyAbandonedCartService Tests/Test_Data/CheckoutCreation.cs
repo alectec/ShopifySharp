d1125971525bce3657813cfba5399960c09531e7
Jamie Hurley <jameshurley@hotmail.com>
2017-06-06 23:37:06 -0400

    ShopifyAbandonedCartService test stubs

---
+++ ShopifySharp.Tests/ShopifyAbandonedCartService Tests/Test_Data/CheckoutCreation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ShopifySharp.Tests.ShopifyAbandonedCartService_Tests.Test_Data
+{
+    public static class CheckoutCreation
+    {
+        public static IEnumerable<ShopifyAbandonedCheckout> CreateCheckouts()
+        {
+            var json = System.IO.File.ReadAllText("checkout.json");
+
+            var checkouts = JsonConvert.DeserializeObject(json, typeof(List<ShopifyAbandonedCheckout>));
+
+            return checkouts as List<ShopifyAbandonedCheckout>;
+        }
+        
+        
+    }
+}
---
+++ ShopifySharp.Tests/ShopifyAbandonedCartService Tests/when_counting_checkouts.cs
@@ -0,0 +1,36 @@
+﻿using Machine.Specifications;
+using System.Collections.Generic;
+
+namespace ShopifySharp.Tests.ShopifyAbandonedCartService_Tests
+{
+    [Subject(typeof(ShopifyAbandonedCartService))]
+    class when_counting_checkouts
+    {
+        Establish context = () =>
+        {
+            Service = new ShopifyAbandonedCartService(Utils.MyShopifyUrl, Utils.AccessToken);
+        };
+
+        Because of = () =>
+        {
+            Result = Service.CountAsync().Await().AsTask.Result;
+        };
+
+        It should_count_checkouts = () =>
+        {
+            Result.ShouldBeGreaterThanOrEqualTo(1);
+        };
+
+
+        Cleanup after = () =>
+        {
+
+        };
+
+        static ShopifyAbandonedCartService Service;
+
+        static List<ShopifyAbandonedCheckout> Created = new List<ShopifyAbandonedCheckout>();
+
+        static int Result;
+    }
+}
